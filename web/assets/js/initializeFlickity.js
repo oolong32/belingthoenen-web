c768ccc7e0b70b47d8027aeb4256f81017375579
Josef Renner <josef@josef.home>
2019-01-10 16:46:48 +0100

    a lot of fruitless stuff

--- web/assets/js/initializeFlickity.js
+++ web/assets/js/initializeFlickity.js
@@ -5,5 +5,7 @@ var flkty = new Flickity( elem, {
   contain: true,
   pageDots: false,
   wrapAround: true,
+  autoPlay: 5000,
+  cellAlign: 'center',
   setGallerySize: false // https://flickity.metafizzy.co/options.html#setgallerysize
 });
