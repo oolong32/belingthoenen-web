87ee80ff0e259f1e3f1d3f49fb6478127b9b919f
Josef Renner <josef@josef.home>
2019-01-14 11:44:08 +0100

    Neuorganisation Layout

--- web/assets/js/project-info.js
+++ web/assets/js/project-info.js
@@ -1,10 +1,34 @@
 // event handlers
+/*
 $('video').click(function() {
   $('.description').removeClass('hidden'); 
+  $('.is-selected').style('height: ' + 800 +'px');
   $('footer').addClass('hidden'); 
 });
+*/
+let videos = document.querySelectorAll('video');
 
-$('.close').click(function() {
-  $('.description').addClass('hidden');
-  $('footer').removeClass('hidden'); 
-}); 
+for (let i = 0; i < videos.length; i++) {
+  videos[i].addEventListener('click' || 'touchend', handleClick);
+}
+
+function handleClick(e) {
+  let container = e.target.parentElement;
+  if (container.classList.contains('is-selected')) {
+    let description = container.children[1];
+    description.classList.remove('hidden');
+
+    // hide archive
+    // container vergrössern etc.
+
+    // attach event listener to close button
+    let close = description.children[0];
+    close.addEventListener('click', hideDescription, {once: true})
+  }
+}
+
+function hideDescription(e) {
+  // Projektbeschrieb ausblenden
+  let description = e.target.parentElement;
+  description.classList.add('hidden');
+}
