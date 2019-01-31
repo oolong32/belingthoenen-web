90d9ea0dd8d6e4108c1d8ed7ef5d0673a0f69687
Josef Renner <josef@josef.home>
2019-01-31 16:04:26 +0100

    unification of gallery layout

--- web/assets/js/initializeFlickity.js
+++ web/assets/js/initializeFlickity.js
@@ -8,12 +8,10 @@ var flkty = new Flickity( elem, {
     }
   },
   cellAlign: 'center', // Wrap-Around funktioniert nicht (Lücke)
-
-  // contain: true,
   pageDots: true,
   wrapAround: true,
   autoPlay: 5000,
-  setGallerySize: false, // https://flickity.metafizzy.co/options.html#setgallerysize
+  //setGallerySize: false, // https://flickity.metafizzy.co/options.html#setgallerysize
   arrowShape: 'M31,58 L78,9 C80.6666667,6.33333333 81,4 79,2 C77,9.32587341e-14 74.3333333,0.333333333 71,3 L24,52 C22,54 21,56 21,58 C21,60 22,62 24,64 L61,97 C64.3333333,99.6666667 67,100 69,98 C71,96 70.6666667,93.3333333 68,90 L31,58 Z'
 });
 
