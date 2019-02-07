08f76152818f3d771e23827824299ab0092961aa
Josef Renner <josef@josef.home>
2019-02-07 16:09:25 +0100

    add test content to archive description

--- web/assets/js/handleArchiveEvents.js
+++ web/assets/js/handleArchiveEvents.js
@@ -2,14 +2,14 @@
 // Thumbs have click/touchend event listeners that toggle visibility of description modal.
 // A visible description modal has a one-time click/touchend event listener that makes it disapear.
 
-const archive = document.querySelectorAll('.archive-project');
+var archive = document.querySelectorAll('.archive-project');
 
-for (let i = 0; i < archive.length; i++ ) {
-  let archiveProject = archive[i];
+for (var i = 0; i < archive.length; i++ ) {
+  var archiveProject = archive[i];
   console.log('project: ' + archiveProject);
   archiveProject.addEventListener('click' || 'touchend', (e) => {
     console.log('target: ' + e.target);
-    let description = archiveProject.querySelector('.archive-project-description');
+    var description = archiveProject.querySelector('.archive-project-description');
     description.classList.remove('hidden');
     window.setTimeout(() => description.classList.add('hidden'), 400 );
     // description.addEventListener('click' || 'touchend', (e) => {
