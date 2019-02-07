2ac529d491441b716726d54b4f0d3280660b2f54
Josef Renner <josef@josef.home>
2019-02-07 17:41:17 +0100

    archive event handling (description modal)

--- web/assets/js/handleArchiveEvents.js
+++ web/assets/js/handleArchiveEvents.js
@@ -2,20 +2,46 @@
 // Thumbs have click/touchend event listeners that toggle visibility of description modal.
 // A visible description modal has a one-time click/touchend event listener that makes it disapear.
 
-var archive = document.querySelectorAll('.archive-project');
 
-for (var i = 0; i < archive.length; i++ ) {
-  var archiveProject = archive[i];
-  console.log('project: ' + archiveProject);
-  archiveProject.addEventListener('click' || 'touchend', (e) => {
-    console.log('target: ' + e.target);
-    var description = archiveProject.querySelector('.archive-project-description');
-    description.classList.remove('hidden');
-    window.setTimeout(() => description.classList.add('hidden'), 400 );
-    // description.addEventListener('click' || 'touchend', (e) => {
-    //   console.log(description);
-    //   description.classList.add('hidden');
-    // }, {once: true});
-  });
+document.addEventListener("DOMContentLoaded", function(event) {
+  var archive = document.querySelectorAll('.archive-project');
+  var main = document.querySelector('main');
+  console.log(archive);
+
+  for (var i = 0; i < archive.length; i++ ) {
+    archive[i].addEventListener('click' || 'touchend', handleArchiveClick);
+  }
+
+  /*
+  for (var i = 0; i < archive.length; i++ ) {
+    var archiveProject = archive[i];
+    console.log('project: ' + archiveProject);
+
+    archiveProject.addEventListener('click' || 'touchend', (e) => {
+      console.log('target: ' + e.target);
+      var description = archiveProject.querySelector('.archive-project-description');
+      description.classList.remove('hidden');
+
+      var close = description.querySelector('.close');
+      close.addEventListener('click' || 'touchend', hideArchiveDescription, {once: true});
+    });
+  }
+  */
+});
+
+function handleArchiveClick(e) {
+  var archiveDescription = e.target.parentElement.children[1];
+  archiveDescription.classList.remove('hidden');
+  var close = archiveDescription.querySelector('.close');
+  close.addEventListener('click' || 'touchend', hideArchiveDescription, {once: true})
+  
+  //main.style.display = 'none';
 }
 
+function hideArchiveDescription(e) {
+  // Archiv-Projektbeschrieb ausblenden
+  var description = e.target.parentElement;
+  description.classList.add('hidden');
+
+  //main.style.display = 'block';
+}
