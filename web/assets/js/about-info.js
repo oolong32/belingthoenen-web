5f0e55923bf1dccc04bf6a13b09a6bb8e1a9896e
Josef Renner <josef.renner@gmail.com>
2019-02-13 17:24:54 +0100

    add basic event handling to about slider

--- web/assets/js/about-info.js
+++ web/assets/js/about-info.js
@@ -2,41 +2,51 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var teamMembers = document.querySelectorAll('.team-member');
-  console.log(teamMembers);
   var footer = document.querySelector('footer');
   var dots = document.querySelector('.flickity-page-dots');
 
   for (var i = 0; i < teamMembers.length; i++) {
     teamMembers[i].addEventListener('click' || 'touchend', handleClick);
   }
-});
 
-function handleClick(e) {
-  var container = e.target;
-  if (container.classList.contains('is-selected')) {
-    var description = container.children[1]; // +1 Exp. for daring to hardcode this.
-    console.log(description);
-    description.classList.remove('hidden');
+  function handleClick(e) {
+    var container = e.target;
 
-    // hide footer
-    footer.classList.add('hidden');
-    // hide dots
-    dots.classList.add('hidden');
+    if (container.classList.contains('is-selected')) {
+      var name = container.children[0]; // +1 Exp. for daring to hardcode this.
 
-    // attach event listener to close button
-    var close = description.querySelector('.close');
-    close.addEventListener('click' || 'touchend', hideDescription, {once: true})
+      // show contact-infos
+      var description = container.children[1]; // +1 Exp. for daring to hardcode this.
+      description.classList.remove('hidden');
+
+      // move name up
+      name.classList.add('up');
+
+      // hide footer
+      footer.classList.add('hidden');
+      // hide dots
+      dots.classList.add('hidden');
+
+      // attach event listener to close button
+      var closeButton = description.children[1];
+      closeButton.addEventListener('click' || 'touchend', hideDescription, {once: true});
+    }
   }
-}
 
-function hideDescription(e) {
-  // Projektbeschrieb ausblenden
-  var description = e.target.parentElement;
-  description.classList.add('hidden');
-  // hide footer
-  footer.classList.remove('hidden');
-  // show dots
-  dots.classList.remove('hidden');
-}
-
+  function hideDescription(e) {
+    e.preventDefault();
+    var closeButton = e.target
+    console.log(closeButton);
+    // Projektbeschrieb ausblenden
+    var description = e.target.parentElement;
+    var name = description.parentElement.children[0]; // +1 Exp. for daring to hardcode this.
+    description.classList.add('hidden');
+    // move name down
+    name.classList.remove('up');
+    // show footer
+    footer.classList.remove('hidden');
+    // show dots
+    dots.classList.remove('hidden');
+  }
+});
 
