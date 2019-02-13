50b860d2c1044158974a49cea8175a5cb5d15dee
Josef Renner <josef@josef.home>
2019-02-13 10:39:05 +0100

    wrap script in domContentLoaded event listener

--- web/assets/js/handleArrowButtons.js
+++ web/assets/js/handleArrowButtons.js
@@ -1,20 +1,23 @@
 // show/hide arrows on mouse-enter/-leave of carousel
 
-let carousel = document.querySelector('.carousel');
-let mouse_arrows = document.querySelectorAll('.flickity-prev-next-button');
+document.addEventListener("DOMContentLoaded", function(event) {
+  var carousel = document.querySelector('.carousel');
+  var mouse_arrows = document.querySelectorAll('.flickity-prev-next-button');
 
-carousel.addEventListener('mouseover', showArrows);
-carousel.addEventListener('mouseleave', hideArrows);
-hideArrows(); // hide arrows at first;
+  carousel.addEventListener('mouseover', showArrows);
+  carousel.addEventListener('mouseleave', hideArrows);
+  hideArrows(); // hide arrows at first;
 
-function hideArrows() {
-  for (arrow of mouse_arrows) {
-    arrow.classList.add('hidden');
+  function hideArrows() {
+    for (arrow of mouse_arrows) {
+      arrow.classList.add('hidden');
+    }
   }
-}
 
-function showArrows() {
-  for (arrow of mouse_arrows) {
-    arrow.classList.remove('hidden');
+  function showArrows() {
+    for (arrow of mouse_arrows) {
+      arrow.classList.remove('hidden');
+    }
   }
-}
+});
+
