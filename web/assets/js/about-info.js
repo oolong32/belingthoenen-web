8a265be9bddc952995e567d33b9f007fb8cc2151
Josef Renner <josef@josef.home>
2019-02-14 15:59:42 +0100

    add comments for readability

--- web/assets/js/about-info.js
+++ web/assets/js/about-info.js
@@ -1,25 +1,38 @@
-// event handlers
+// event handlers for about slider
+// ===============================
+// touch or click on slider-slide (team member image)
+// shows/hides additional infos
+// and hides/shows slider-dots and footer (mobile only)
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+  // list of elements that need an event listener
+  // i.e. all slides containing an image of a team member
   var teamMembers = document.querySelectorAll('.team-member');
+
+  // visible elements that need to go in case of event
   var footer = document.querySelector('footer');
   var dots = document.querySelector('.flickity-page-dots');
 
+  // attach event listeners
   for (var i = 0; i < teamMembers.length; i++) {
     teamMembers[i].addEventListener('click' || 'touchend', handleClick);
   }
 
   function handleClick(e) {
+
+    // get the slide containing the hidden information, in
+    // this case a slide with a background image of the team member
     var container = e.target;
 
     if (container.classList.contains('is-selected')) {
-      var name = container.children[0]; // +1 Exp. for daring to hardcode this.
+      var name = container.children[0]; // +1 Exp. for daring to hardcode this
 
-      // show contact-infos
-      var description = container.children[1]; // +1 Exp. for daring to hardcode this.
+      // display infos
+      var description = container.children[1]; // +5 Exp.
       description.classList.remove('hidden');
 
-      // move name up
+      // move name up to accomodate the unhidden infos
       name.classList.add('up');
 
       // hide footer
@@ -34,17 +47,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function hideDescription(e) {
-    //e.preventDefault();
+
+    // not neccessary, but better safe than sorry
+    e.preventDefault();
+
+    // hide infos
     var closeButton = e.target
-    console.log(closeButton);
-    // Projektbeschrieb ausblenden
     var description = e.target.parentElement;
-    var name = description.parentElement.children[0]; // +1 Exp. for daring to hardcode this.
     description.classList.add('hidden');
-    // move name down
+
+    // move name down again
+    var name = description.parentElement.children[0]; // +100 Exp. & level up!
     name.classList.remove('up');
+
     // show footer
     footer.classList.remove('hidden');
+
     // show dots
     dots.classList.remove('hidden');
   }
--- web/assets/js/project-info.js
+++ web/assets/js/project-info.js
@@ -1,17 +1,32 @@
-// event handlers
+// event handlers for home slider
+// ==============================
+// touch or click on slider-card (project movie)
+// shows/hides additional infos
+// and hides/shows slider-dots and footer (mobile only)
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+  // list of elements that need an event listener
+  // i.e. all slides (containing a project video)
   var videos = document.querySelectorAll('video');
+
+  // visible elements that need to go in case of event
   var footer = document.querySelector('footer');
   var dots = document.querySelector('.flickity-page-dots');
 
+  // attach event listener
   for (var i = 0; i < videos.length; i++) {
     videos[i].addEventListener('click' || 'touchend', handleClick);
   }
 
   function handleClick(e) {
+
+    // get the slide containing the video and the hidden infos
     var container = e.target.parentElement;
+
     if (container.classList.contains('is-selected')) {
+
+      // display infos
       var description = container.children[1]; // +1 Exp. for daring to hardcode this.
       description.classList.remove('hidden');
 
@@ -27,12 +42,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function hideDescription(e) {
+
+    // not neccessary, but better safe than sorry
     e.preventDefault();
-    // Projektbeschrieb ausblenden
-    var description = e.target.parentElement;
+
+    // hide infos
+    var closeButton = e.target;
+    var description = closeButton.parentElement;
     description.classList.add('hidden');
-    // hide footer
+
+    // show footer
     footer.classList.remove('hidden');
+
     // show dots
     dots.classList.remove('hidden');
   }
