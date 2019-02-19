6f5595874932d3fb52f1a786f14d2137fc020de9
Josef Renner <josef@josef.home>
2019-02-19 17:48:45 +0100

    rewrite archive functionality

--- web/assets/js/handleArchiveEvents.js
+++ web/assets/js/handleArchiveEvents.js
@@ -4,80 +4,79 @@
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
   var archive = document.querySelectorAll('.archive-project');
+  var descriptions = document.querySelectorAll('.archive-project-description');
 
   for (var i = 0; i < archive.length; i++ ) {
     archive[i].addEventListener('click' || 'touchend', handleArchiveClick);
+    // record order to find matching description
+    archive[i].archiveOrder = i;
   }
 
-  /*
-  for (var i = 0; i < archive.length; i++ ) {
-    var archiveProject = archive[i];
-    console.log('project: ' + archiveProject);
-
-    archiveProject.addEventListener('click' || 'touchend', (e) => {
-      console.log('target: ' + e.target);
-      var description = archiveProject.querySelector('.archive-project-description');
-      description.classList.remove('hidden');
-
-      var close = description.querySelector('.close');
-      close.addEventListener('click' || 'touchend', hideArchiveDescription, {once: true});
-    });
-  }
-  */
-});
-
-function handleArchiveClick(e) {
-  if (e.target.classList.contains('archive-project-description')) {
-    // clicked on the description
-    // such click should not close the modal
-    return true;
-  }
-  // console.log(e.target);
-  var archiveDescription = e.target.parentElement.children[1];
-  archiveDescription.classList.remove('hidden');
-  var close = archiveDescription.querySelector('.close');
-  close.addEventListener('click' || 'touchend', hideArchiveDescription, {once: true})
-
-
-  // parent list shall grow to display all it’s wonderful content
-  var parentLi = archiveDescription.parentElement;
-  var parentUl = parentLi.parentElement;
-  var allLiItems = parentUl.children;
-  var footer = document.querySelector('footer');
-  var marquee = document.querySelector('.simple-marquee-container');
-  // check what’s active and reset it
-  for (var i = 0; i < allLiItems.length; i++) {
-    if (allLiItems[i].classList.contains('active')) { // there was already an active archive project
-      var activeLi = allLiItems[i];
-      activeLi.classList.remove('active');
-      var activeDescription = activeLi.children[1];
-      activeDescription.classList.add('hidden');
-      // var activeClose = activeDescription.children[2];
-      // activeClose.removeEventListener('click' || 'touchend', hideArchiveDescription);
-      // this doesn’t work, let’s hope it doesn’t cause trouble
-
+  function handleArchiveClick(e) {
+    if (e.target.classList.contains('archive-project-description')) {
+      // clicked on the description
+      // such click should not close the modal
+      return true;
     }
+    // console.log(e.target);
+
+    //var archiveDescription = e.target.parentElement.children[1];
+    var archiveDescription = descriptions[e.target.parentElement.archiveOrder];
+
+    archiveDescription.classList.remove('hidden');
+    var close = archiveDescription.querySelector('.close');
+    close.addEventListener('click' || 'touchend', hideArchiveDescription, {once: true})
+
+
+    // parent list shall grow to display all it’s wonderful content
+    var clickedThumb = e.target.parentElement; 
+    var archiveThumbsUl = clickedThumb.parentElement; 
+    var descriptionUl = document.querySelector('#archive-descriptions'); 
+    var allArchiveThumbs = archiveThumbsUl.children; 
+    var footer = document.querySelector('footer');
+    var marquee = document.querySelector('.simple-marquee-container');
+    // check what’s active and reset it
+    for (var i = 0; i < allArchiveThumbs.length; i++) {
+      if (allArchiveThumbs[i].classList.contains('active')) { // there was already an active archive project
+        var activeThumb = allArchiveThumbs[i];
+        activeThumb.classList.remove('active');
+        var activeDescription = descriptions[activeThumb.archiveOrder];
+        activeDescription.classList.add('hidden');
+        // var activeClose = activeDescription.children[2];
+        // activeClose.removeEventListener('click' || 'touchend', hideArchiveDescription);
+        // this doesn’t work, let’s hope it doesn’t cause trouble
+
+      }
+    }
+    // set everything involved to active 
+    descriptionUl.classList.remove('hidden'); // description container becomes visible
+    archiveThumbsUl.classList.add('active'); // thumb-row doesn’t collapse on mouse out
+    clickedThumb.classList.add('active'); // thumb grows
+    footer.classList.add('active');  // ?
+    marquee.classList.add('hidden'); // hide news ticker
   }
-  // set everything involved to active 
-  parentUl.classList.add('active');
-  parentLi.classList.add('active');
-  footer.classList.add('active');
-  marquee.classList.add('hidden');
-}
 
-function hideArchiveDescription(e) {
-  // Archiv-Projektbeschrieb ausblenden
-  var description = e.target.parentElement;
-  description.classList.add('hidden');
+  function hideArchiveDescription(e) {
+    // hide archive poject description container
+    var archiveProjectDescriptionContainer = document.querySelector('#archive-descriptions');
+    archiveProjectDescriptionContainer.classList.add('hidden');
+    // hide archive project description
+    var description = e.target.parentElement;
+    description.classList.add('hidden');
 
-  // parent list shall shrink
-  var parentLi = description.parentElement;
-  var parentUl = parentLi.parentElement;
-  var footer = document.querySelector('footer');
-  var marquee = document.querySelector('.simple-marquee-container');
-  parentLi.classList.remove('active');
-  parentUl.classList.remove('active');
-  footer.classList.remove('active');
-  marquee.classList.remove('hidden');
-}
+    // parent list shall shrink
+    var archiveThumbsUl = document.querySelector('#archive ul');
+    var archiveThumbs = document.querySelector('#archive-project');
+    var footer = document.querySelector('footer');
+    var marquee = document.querySelector('.simple-marquee-container');
+    for (var i = 0; i < archiveThumbsUl.length; i++) {
+      archiveThumbs[i].classList.remove('active');
+    }
+    archiveThumbsUl.classList.remove('active');
+    footer.classList.remove('active');
+    marquee.classList.remove('hidden');
+  }
+
+});
