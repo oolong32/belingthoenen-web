0dd873e58b49b0fd830a4edf9c9b0c588faeb85f
Josef Renner <josef@josef.home>
2019-03-05 10:02:00 +0100

    rework arrow handler, show when mouseY over main

--- web/assets/js/handleArrowButtons.js
+++ web/assets/js/handleArrowButtons.js
@@ -1,24 +1,46 @@
 // show/hide arrows on mouse-enter/-leave of carousel
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  var carousel = document.querySelector('.carousel');
-  var mouse_arrows = document.querySelectorAll('.flickity-prev-next-button');
 
-  carousel.addEventListener('mouseover', showArrows);
-  carousel.addEventListener('mouseleave', hideArrows);
-  hideArrows(); // hide arrows at first;
+  var mouse_arrows = document.querySelectorAll('.flickity-prev-next-button');
+  var main = document.querySelector('main');
+
+  // hide carousel arrows at first;
+  hideArrows();
+
+  // on mouse movement, check if mouse above main
+  // if so, show carousel arrows
+  document.addEventListener('mousemove', function(event) {
+    
+    // get position of main
+    var mainPos = main.getBoundingClientRect();
+
+    mousePos = {
+            x: event.pageX,
+            y: event.pageY
+        };
+    if (mousePos.y > mainPos.top && mousePos.y < mainPos.bottom) {
+      showArrows();
+    } else {
+      hideArrows();
+    }
+  });
 
   function hideArrows() {
     for (var i = 0; i < mouse_arrows.length; i++) {
       var arrow = mouse_arrows[i];
-      arrow.classList.add('hidden');
+      if (!arrow.classList.contains('hidden')) {
+          arrow.classList.add('hidden');
+      }
     }
   }
 
   function showArrows() {
     for (var i = 0; i < mouse_arrows.length; i++) {
       var arrow = mouse_arrows[i];
-      arrow.classList.remove('hidden');
+      if (arrow.classList.contains('hidden')) {
+        arrow.classList.remove('hidden');
+      }
     }
   }
 });
