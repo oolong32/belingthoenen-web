83220f9e3f99d54303e565ffbda4d72c7f28ed68
Josef Renner <josef@josef.home>
2019-03-05 17:04:46 +0100

    set new breakpoints

--- web/assets/js/initializeAboutFlickity.js
+++ web/assets/js/initializeAboutFlickity.js
@@ -8,11 +8,11 @@ var flkty = new Flickity( elem, {
   contain: true,
   autoPlay: false,
   cellAlign: 'center',
-  arrowShape: 'M31,58 L78,9 C80.6666667,6.33333333 81,4 79,2 C77,9.32587341e-14 74.3333333,0.333333333 71,3 L24,52 C22,54 21,56 21,58 C21,60 22,62 24,64 L61,97 C64.3333333,99.6666667 67,100 69,98 C71,96 70.6666667,93.3333333 68,90 L31,58 Z'
+  arrowShape: 'M31,58 L78,9 C80.666,6.333 81,4 79,2 C77,9.325 74.333,0.333 71,3 L24,52 C22,54 21,56 21,58 C21,60 22,62 24,64 L61,97 C64.333,99.666 67,100 69,98 C71,96 70.666,93.333 68,90 L31,58 Z'
   //setGallerySize: false // https://flickity.metafizzy.co/options.html#setgallerysize
 });
 
 // Intro Slide to the right on Desktop
-if (window.innerWidth >= 768) { // Desktop Viewport
+if (window.innerWidth >= 1000) { // Desktop Viewport
   flkty.select(1, false, true);
 }
